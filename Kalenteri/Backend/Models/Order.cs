3df77bd17c49ed0847a44ef0eb029ecffb1bff48
Veikko Lintujärvi <veikko@lintujarvi.fi>
2024-01-24 21:18:40 +0200

    Initial backend structure

---
+++ Kalenteri/Backend/Models/Order.cs
@@ -0,0 +1,15 @@
+
+namespace Kalenteri.Backend.Models;
+
+public class Order
+{
+    public string Identifier { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public List<Box> Boxes { get; set; } = new List<Box>();
+}
+
+public class Box
+{
+    public DateTime Delivery { get; set; }
+    public DateTime PickUp { get; set; }
+}
---
+++ Kalenteri/Backend/Services/OrderService.cs
@@ -0,0 +1,7 @@
+namespace Kalenteri.Backend.Services;
+
+public class OrderServices
+{
+    // Business logic here
+}
+
