c3f7a7ddd1cd4c4af53d4648d1cf67c8a49ff907
Jani Pulkkinen <jani.pulkkinen@cloubi.com>
2024-06-17 10:37:57 +0300

    Ensimmäinen versio.

--- Kalenteri/Backend/Models/Order.cs
+++ Kalenteri/Backend/Models/Order.cs
@@ -1,16 +1,30 @@
 
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
 namespace Kalenteri.Backend.Models;
 
+[Alias("kalenteri_order")] 
 public class Order
 {
-    public string id { get; set; } = string.Empty;
+    public Guid id { get; set; } = Guid.Empty;
     public string Identifier { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
+    [Reference]
     public List<Box> Boxes { get; set; } = new List<Box>();
 }
 
+[Alias("kalenteri_box")]
 public class Box
 {
+    public Guid id { get; set; } = Guid.Empty;
+
+    [Alias("order_id")] 
+    public Guid OrderId { get; set; }
+    
+    [Alias("delivery_time")] 
     public DateTime Delivery { get; set; }
+    
+    [Alias("pickup_time")] 
     public DateTime? PickUp { get; set; }
 }
